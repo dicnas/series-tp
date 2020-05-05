afa77f2d6cdaf1dbffcd015531e51bfc1f1ae86e
Bryan <bryan@bryan.com>
2020-05-05 15:50:54 -0400

    cargado categoria

--- main.js
+++ main.js
@@ -5,6 +5,16 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.get('/category', function (req, res) {
+
+  res.send([
+    { title: "TVshows", desc: "ingresar imagen" },
+    { title: "Peliculas", desc: "ingresar imagen1" },
+    { title: "TVshows", desc: "ingresar imagen" },
+    { title: "JAQUIShoes", desc: "ingrasdasesar imagen" }
+  ]);
+});
+
 app.use(express.static('public'));
 
 app.listen(process.env.PORT, function () {
